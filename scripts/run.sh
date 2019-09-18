81fe7e11679aaa8bbc2c61aeba6a3b48e4453651
A-Tsai <chungtsai@gmail.com>
2019-09-18 17:02:33 +0800

    run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+#####################################################
+## NOTE: Please modify these variables before running
 SPARK=/usr/local/spark
 SPARK_MASTER=server
 CONNECTEDREADS_FOLDER=/usr/local/connectedreads
 JAR_FOLDER=/seqslab
+#####################################################
+
+## local disk
 ADAM_JAR=${JAR_FOLDER}/adam-assembly-1.0.0-qual.jar
 CONNECTEDREADS_JAR=${JAR_FOLDER}/connectedreads-1.0.0.jar
 READ1=./NA19240/SRR7782669_pass_1.fastq.gz
 READ2=./NA19240/SRR7782669_pass_2.fastq.gz
+OUTPUT_FASTQ=./NA19240/SRR7782669_ConnectedReads.fastq.gz
+## HDFS
 HDFS_OUTPUT=./NA19240
 CHUNK_FOLDER=${HDFS_OUTPUT}/chunk.fq
 ADAM_FOLDER=${HDFS_OUTPUT}/adam
@@ -17,7 +24,6 @@ SG_CHECKPOINT_FOLDER=${HDFS_OUTPUT}/sg-checkpoint
 HSA_FOLDER=${HDFS_OUTPUT}/hsa
 HSA_CHECKPOINT_FOLDER=${HDFS_OUTPUT}/hsa-checkpoint
 TMP_FOLDER=${HDFS_OUTPUT}/tmp
-OUTPUT_FASTQ=./NA19240/SRR7782669_ConnectedReads.fastq.gz
 
 hadoop fs -rm -r ${CHUNK_FOLDER}
 
